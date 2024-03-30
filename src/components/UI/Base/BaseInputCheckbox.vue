c0a6854fdf682a1e89d4128485b92bc9352d80a4
LlONED <alex5000843@gmail.com>
2024-03-30 15:18:16 +0300

    Added BaseInputCheckbox and BaseInputRadio

---
+++ src/components/UI/Base/BaseInputCheckbox.vue
@@ -0,0 +1,68 @@
+<script setup>
+const emit = defineEmits(["change"]);
+const { checked } = defineProps({
+  checked: Boolean,
+});
+</script>
+
+<template>
+  <label class="label relative cursor-pointer">
+    <input
+      @change="emit('change', $event.target.checked)"
+      :checked="checked"
+      type="checkbox"
+      class="input"
+    />
+    <span class="mask"></span>
+  </label>
+</template>
+
+<style scoped>
+.label {
+  position: relative;
+  cursor: pointer;
+}
+
+.input {
+  position: absolute;
+  z-index: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  appearance: none;
+  outline: none;
+}
+
+.mask {
+  border: 1px solid var(--color-border);
+  position: absolute;
+
+  width: 100%;
+  height: 100%;
+
+  border-radius: 8px;
+}
+
+.input:checked + .mask {
+  background-color: var(--green-soft);
+  transition: 0.2s background-color ease;
+}
+
+.input:checked:hover + .mask,
+.input:checked:focus + .mask {
+  background-color: var(--green-mute);
+  transition: 0.2s background-color ease;
+}
+
+.input:not(:checked) + .mask {
+  background-color: var(--red-soft);
+  transition: 0.2s background-color ease;
+}
+
+.input:not(:checked):hover + .mask,
+.input:not(:checked):focus + .mask {
+  background-color: var(--red-mute);
+  transition: 0.2s background-color ease;
+}
+</style>
---
+++ src/components/UI/Base/BaseInputRadio.vue
@@ -0,0 +1,56 @@
+<script setup>
+const emit = defineEmits(["update:modelValue"]);
+const { modelValue, value } = defineProps({
+  modelValue: String,
+  value: String,
+});
+</script>
+
+<template>
+  <label class="input-label">
+    <span>{{ value }}</span>
+
+    <input
+      @change="emit('update:modelValue', $event.target.value)"
+      :checked="modelValue === value"
+      :value="value"
+      type="radio"
+      class="input"
+    />
+  </label>
+</template>
+
+<style scoped>
+.input-label {
+  position: relative;
+  padding: 4px 8px;
+  border-radius: 4px;
+  overflow: hidden;
+}
+
+.input {
+  appearance: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  background-color: var(--green);
+  transition: 0.2s background-color ease;
+}
+
+.input:not(:checked) {
+  cursor: pointer;
+}
+
+.input:checked {
+  background-color: var(--green-light);
+  transition: 0.2s background-color ease;
+}
+
+.input:hover {
+  background-color: var(--green-soft);
+  transition: 0.2s background-color ease;
+}
+</style>
