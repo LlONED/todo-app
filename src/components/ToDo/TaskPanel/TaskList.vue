17f904949d6e0eedd4c6de9e19f4ffb19a3d9c58
LlONED <alex5000843@gmail.com>
2024-03-30 15:27:06 +0300

    Added processing of an empty array in TaskList

--- src/components/ToDo/TaskPanel/TaskList.vue
+++ src/components/ToDo/TaskPanel/TaskList.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { tasksFilterEnum } from "@/helpers/constants/tasks";
 import TaskListItem from "./TaskListItem.vue";
 
 import { useTasksStore } from "@/stores/tasks.js";
@@ -7,17 +8,32 @@ const tasksStore = useTasksStore();
 </script>
 
 <template>
-  <ul class="task-list">
+  <ul v-if="tasksStore.filteredTasks.length" class="task-list">
     <TaskListItem
       v-for="task in tasksStore.filteredTasks"
       :key="task.id"
       :name="task.name"
       :description="task.description"
       :done="task.done"
-      @updateDone="tasksStore.updateTaskDone(task.id, $event)"
-      @remove="tasksStore.removeTask(task.id)"
     />
   </ul>
+  <p
+    v-else-if="
+      tasksStore.filter === tasksFilterEnum.FINISHED &&
+      tasksStore.list.length > 0
+    "
+  >
+    Выполненных заданий нет (′⌒`)
+  </p>
+  <p
+    v-else-if="
+      tasksStore.filter === tasksFilterEnum.UNFINISHED &&
+      tasksStore.list.length > 0
+    "
+  >
+    Все задания выполнены \(@^0^@)/
+  </p>
+  <p v-else>Заданий нет ¯\_(ツ)_/¯</p>
 </template>
 
 <style scoped>
